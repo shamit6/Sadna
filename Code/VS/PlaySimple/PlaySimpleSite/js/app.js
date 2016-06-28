a32ec91f396ea7c6fe3e55fcfccfc5ef6d2a37ee
krasilies <giladsegal5@gmail.com>
2016-06-28 21:31:49 +0300

    made some partials work

--- Code/VS/PlaySimple/PlaySimpleSite/js/app.js
+++ Code/VS/PlaySimple/PlaySimpleSite/js/app.js
@@ -3,20 +3,25 @@
     var playSimpleApp = angular.module('myApp', ['ngRoute']);
 
     playSimpleApp.config(['$routeProvider', function ($routeProvider) {
-        $routeProvider.when('/view1', {
-            templateUrl: 'partials/partial1.html',
+        $routeProvider.when('/login', {
+            templateUrl: 'partials/loginForm.html',
             controller: 'MyCtrl1'
         });
 
-        $routeProvider.when('/view2', {
-            templateUrl: 'partials/partial2.html',
+        $routeProvider.when('/ownedInvitations', {
+            templateUrl: 'partials/ownedInvitations.html',
+            controller: 'MyCtrl1'
+        });
+
+        $routeProvider.when('/registration', {
+            templateUrl: 'partials/registrationForm.html',
             controller: 'MyCtrl2'
         });
 
         $routeProvider.otherwise({
-            redirectTo: '/view1'
+            redirectTo: '/login'
         });
     }]);
 })();
 
-
+ 
