48afd801fd2376ecb46c934c4000f82ae296d7a8
krasilies <giladsegal5@gmail.com>
2016-09-06 23:00:13 +0300

    added decodes

--- Code/VS/PlaySimple/PlaySimple/js/services.js
+++ Code/VS/PlaySimple/PlaySimple/js/services.js
@@ -1,5 +1,99 @@
 ﻿!(function () {
     myApp = angular.module('myApp');
-    myApp.value('version', '0.1');
+    myApp.factory("MyDecodes", function(){
+        return {
+            orderStatus: [
+            {
+                id: 1,
+                name: 'נשלח'
+            },
+            {
+                id: 2,
+                name: 'התקבל'
+            },
+            {
+                id: 3,
+                name: 'נדחה'
+            },
+            {
+                id: 4,
+                name: 'בוטל'
+            }
+            ],
+            complaintType: [
+                {
+                    id: 1,
+                    name: 'אי תשלום'
+                },
+                {
+                    id: 2,
+                    name: 'אי הגעה'
+                },
+                {
+                    id: 3,
+                    name: 'חוסר ספורטיביות'
+                }
+            ],
+            fieldSize: [
+                {
+                    id: 1,
+                    name: 'קטן'
+                },
+                {
+                    id: 2,
+                    name: 'בינוני'
+                },
+                {
+                    id: 3,
+                    name: 'גדול'
+                }
+            ],
+            fieldType: [
+                {
+                    id: 1,
+                    name: 'כדורגל'
+                },
+                {
+                    id: 2,
+                    name: 'כדורסל'
+                },
+                {
+                    id: 3,
+                    name: 'טניס'
+                }
+            ],
+            invitationStatus: [
+                {
+                    id: 1,
+                    name: 'נשלח'
+                },
+                {
+                    id: 2,
+                    name: 'התקבל'
+                },
+                {
+                    id: 3,
+                    name: 'נדחה'
+                }
+            ],
+            regionDecode: [
+                {
+                    id: 1,
+                    name: 'דן'
+                },
+                {
+                    id: 2,
+                    name: 'נגב'
+                },
+                {
+                    id: 3,
+                    name: 'חיפה'
+                },
+                {
+                    id: 4,
+                    name: 'ירושלים'
+                }
+         ]};
+    });
 })();
 
