a901c69de462e7a3de24ff95b90bf6e54dd2137e
krasilies <giladsegal5@gmail.com>
2016-09-11 22:47:22 +0300

    sub menus

--- Code/VS/PlaySimple/PlaySimple/js/app.js
+++ Code/VS/PlaySimple/PlaySimple/js/app.js
@@ -16,83 +16,90 @@
             templateUrl: 'partials/registrationForm.html',
             controller: 'RegistrationFormCtrl'
         });
-        //$routeProvider.when('/searchCustomers', {
-        //    templateUrl: 'partials/searchCustomers.html',
-        //    controller: 'LoginCtrl'
-        //});
 
         $routeProvider.when('/ownedPendingInvitations', {
             templateUrl: 'partials/ownedPendingInvitations.html',
-            controller: 'LoginCtrl'
+            controller: 'LoginCtrl',
+            activetab: ''
         });
 
-        //$routeProvider.when('/ownedOrders', {
-        //    templateUrl: 'partials/ownedOrders.html',
-        //    controller: 'LoginCtrl'
-        //});
         $routeProvider.when('/editField/:Id?', {
             templateUrl: 'partials/editField.html',
-            controller: 'FieldsCtrl'
+            controller: 'FieldsCtrl',
+            activetab: 'editField'
         });
         $routeProvider.when('/editCustomer/:Id?', {
             templateUrl: 'partials/editCustomer.html',
-            controller: 'CustomersCtrl'
+            controller: 'CustomersCtrl',
+            activetab: ''
         });
         $routeProvider.when('/searchFields', {
             templateUrl: 'partials/reportFields.html',
-            controller: 'SearchFieldsCtrl'
+            controller: 'SearchFieldsCtrl',
+            activetab: 'searchFields'
         });
 
         $routeProvider.when('/editEmployee/:Id?', {
             templateUrl: 'partials/editEmployee.html',
-            controller: 'EmployeesCtrl'
+            controller: 'EmployeesCtrl',
+            activetab: 'editEmployee'
         });
         $routeProvider.when('/searchEmployees', {
             templateUrl: 'partials/searchEmployee.html',
-            controller: 'SearchEmployeesCtrl'
+            controller: 'SearchEmployeesCtrl',
+            activetab: 'searchEmployees'
         });
         $routeProvider.when('/reportCustomer', {
             templateUrl: 'partials/reportCustomer.html',
-            controller: 'ReportCustomerCtrl'
+            controller: 'ReportCustomerCtrl',
+            activetab: 'reports'
         });
         $routeProvider.when('/reportComplaint', {
             templateUrl: 'partials/reportComplaint.html',
-            controller: 'ReportComplaintCtrl'
+            controller: 'ReportComplaintCtrl',
+            activetab: 'reports'
         }); 
         $routeProvider.when('/searchCustomers', {
             templateUrl: 'partials/searchCustomers.html',
-            controller: 'SearchCustomersCtrl'
+            controller: 'SearchCustomersCtrl',
+            activetab: 'searchCustomers'
         }); 
         $routeProvider.when('/searchAvailableOrders', {
             templateUrl: 'partials/searchAvailableOrders.html',
-            controller: 'SearchAvailableOrdersCtrl'
+            controller: 'SearchAvailableOrdersCtrl',
+            activetab: 'searchAvailableOrders'
         });
         $routeProvider.when('/ownedOrders', {
             templateUrl: 'partials/ownedOrders.html',
-            controller: 'ownedOrdersCrtl'
+            controller: 'ownedOrdersCrtl',
+            activetab: 'ownedOrders'
         }); 
         $routeProvider.when('/editOrder/:Id?', {
             templateUrl: 'partials/editOrder.html',
-            controller: 'OrdersCtrl'
+            controller: 'OrdersCtrl',
+            activetab: ''
         }); 
         $routeProvider.when('/searchAvailableOrdersToJoin', {
             templateUrl: 'partials/searchAvailableOrdersToJoin.html',
-            controller: 'SearchAvailableOrdersToJoinCrtl'
+            controller: 'SearchAvailableOrdersToJoinCrtl',
+            activetab: 'searchAvailableOrdersToJoin'
         });
         $routeProvider.when('/searchOrders', {
             templateUrl: 'partials/searchOrders.html',
-            controller: 'SearchOrdersCrtl'
+            controller: 'SearchOrdersCrtl',
+            activetab: 'searchOrders'
         });
         $routeProvider.when('/pendingOrdersToJoin', {
             templateUrl: 'partials/pendingOrdersToJoin.html',
-            controller: 'PendingOrdersToJoinCrtl'
+            controller: 'PendingOrdersToJoinCrtl',
+            activetab: 'pendingOrdersToJoin'
         });
         $routeProvider.otherwise({
             redirectTo: '/login'
         });
     }]);
 
-    playSimpleApp.run(function ($rootScope, $location, LoginService) {
+    playSimpleApp.run(function ($rootScope, $location, $route, LoginService) {
         $rootScope.sharedVariables = {
             isLogin: true
         };
@@ -116,6 +123,7 @@
             LoginService.navigateToHomepage($location.path());
         }
 
+        $rootScope.$route = $route;
         $rootScope.spinnerActive = false;
 
         $rootScope.$on('us-spinner:spin', function (event, key) {
