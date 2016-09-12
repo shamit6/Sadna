d127591084dbe231ca9570812a8785b253738de0
krasilies <giladsegal5@gmail.com>
2016-09-12 23:08:51 +0300

    form validation - forgot something

--- Code/VS/PlaySimple/PlaySimple/js/directives.js
+++ Code/VS/PlaySimple/PlaySimple/js/directives.js
@@ -1,8 +1,31 @@
 ﻿!(function () {
     var myApp = angular.module('myApp');
-    myApp.directive('appVersion', ['version', function (version) {
-          return function (scope, elm, attrs) {
-              elm.text(version);
-          };
-      }]);
+    myApp.directive('equals', function() {
+        return {
+            restrict: 'A', // only activate on element attribute
+            require: '?ngModel', // get a hold of NgModelController
+            link: function(scope, elem, attrs, ngModel) {
+                if(!ngModel) return; // do nothing if no ng-model
+
+                // watch own value and re-validate on change
+                scope.$watch(attrs.ngModel, function() {
+                    validate();
+                });
+
+                // observe the other value and re-validate on change
+                attrs.$observe('equals', function (val) {
+                    validate();
+                });
+
+                var validate = function() {
+                    // values
+                    var val1 = ngModel.$viewValue;
+                    var val2 = attrs.equals;
+
+                    // set validity
+                    ngModel.$setValidity('equals', /*! val1 || ! val2 || */val1 === val2);
+                };
+            }
+        }
+    });
 })();
