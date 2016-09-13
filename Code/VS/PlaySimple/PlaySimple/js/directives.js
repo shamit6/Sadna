65b76eb319b650f3eb7aa801f34b33f508af00b3
krasilies <giladsegal5@gmail.com>
2016-09-13 21:28:49 +0300

    validation!

--- Code/VS/PlaySimple/PlaySimple/js/directives.js
+++ Code/VS/PlaySimple/PlaySimple/js/directives.js
@@ -23,7 +23,65 @@
                     var val2 = attrs.equals;
 
                     // set validity
-                    ngModel.$setValidity('equals', /*! val1 || ! val2 || */val1 === val2);
+                    ngModel.$setValidity('equals', val1 === val2);
+                };
+            }
+        }
+    });
+
+    myApp.directive('past', function () {
+        var today = new Date();
+
+        return {
+            restrict: 'A', // only activate on element attribute
+            require: '?ngModel', // get a hold of NgModelController
+            link: function (scope, elem, attrs, ngModel) {
+                if (!ngModel) return; // do nothing if no ng-model
+
+                // watch own value and re-validate on change
+                scope.$watch(attrs.ngModel, function () {
+                    validate();
+                });
+
+                // observe the other value and re-validate on change
+                attrs.$observe('equals', function (val) {
+                    validate();
+                });
+
+                var validate = function () {
+                    var val = ngModel.$viewValue;
+
+                    // set validity
+                    ngModel.$setValidity('past', new Date(val) < today);
+                };
+            }
+        }
+    });
+
+    myApp.directive('future', function () {
+        var today = new Date();
+
+        return {
+            restrict: 'A', // only activate on element attribute
+            require: '?ngModel', // get a hold of NgModelController
+            link: function (scope, elem, attrs, ngModel) {
+                if (!ngModel) return; // do nothing if no ng-model
+
+                // watch own value and re-validate on change
+                scope.$watch(attrs.ngModel, function () {
+                    validate();
+                });
+
+                // observe the other value and re-validate on change
+                attrs.$observe('equals', function (val) {
+                    validate();
+                });
+
+                var validate = function () {
+                    var val = ngModel.$viewValue;
+
+                    // set validity
+                    ngModel.$setValidity('future', new Date(val) > today);
                 };
             }
         }
