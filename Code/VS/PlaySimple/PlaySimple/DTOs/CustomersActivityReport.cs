b1656ebff9771ccfa5ef571f13aadbea43adf623
shamit6 <amishtalev@gmail.com>
2016-09-26 00:55:40 +0300

    buttons

--- Code/VS/PlaySimple/PlaySimple/DTOs/CustomersActivityReport.cs
+++ Code/VS/PlaySimple/PlaySimple/DTOs/CustomersActivityReport.cs
@@ -17,7 +17,7 @@ namespace PlaySimple.DTOs
         [MaxLength(20)]
         public virtual string LastName { get; set; }
 
-        public virtual int Age { get; set; }
+        public virtual DateTime BirthDate { get; set; }
 
         public virtual long LastGameDate { get; set; }
 
